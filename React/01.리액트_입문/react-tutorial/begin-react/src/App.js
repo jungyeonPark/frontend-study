a57781accdc5b3b40826c4ea8c1bef2b24dfb8ef
jungyeonPark <kshpjy2626@gmail.com>
2021-03-21 03:14:09 +0900

    react 1장 11.배열 렌더링하기까지 정리 완료

--- React/01.리액트_입문/react-tutorial/begin-react/src/App.js
+++ React/01.리액트_입문/react-tutorial/begin-react/src/App.js
@@ -1,13 +1,12 @@
 import Counter from './Counter';
 import Hello from './Hello'; // 뒤에 .js는 생략 가능
 import InputSample from './InputSample';
+import UserList from './UserList';
 import Wrapper from './Wrapper';
 
 function App() {
   return (
-    // isSpecial만 쓰면 자동으로 값이 true가됨 
-
-    <InputSample />
+    <UserList/>
   );
 }
 
